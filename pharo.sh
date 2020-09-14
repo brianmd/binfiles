115e6ce2eba69e087d0cf0e16d70dbc1c80ce9ed
Brian Murphy-Dye <brian.murphy-dye@thetradedesk.com>
2020-09-14 07:41:36 -0600

    Add pharo.sh

---
+++ pharo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# usage: pharo.sh eval "10 factorial"
+/Applications/PharoLauncher.app/Contents/MacOS/Pharo --headless ~/Documents/Pharo/images/Pharo\ 8.0\ -\ 64bit\ \(stable\)/Pharo\ 8.0\ -\ 64bit\ \(stable\).image "$@"
